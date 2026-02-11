94648cca00619cd6316d04b977295d61ab879517
Shehab <sattia@hello-robot.com>
2026-02-11 14:50:45 -0500

    Changed contig.tostring() -> contig.tobytes() because tostring() is deprecated in numpy>=1.19 and removed entirely in newer versions of numpy.

--- ros2_numpy/image.py
+++ ros2_numpy/image.py
@@ -110,7 +110,7 @@ def numpy_to_image(arr, encoding):
 
     # make the array contiguous in memory, as mostly required by the format
     contig = np.ascontiguousarray(arr)
-    im.data = contig.tostring()
+    im.data = contig.tobytes()
     im.step = contig.strides[0]
     im.is_bigendian = (
         arr.dtype.byteorder == '>' or
